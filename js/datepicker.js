54fd30409c85a3427830fbb07c641f2a85ff583b
SheepTester <ussa.say@gmail.com>
2018-06-06 19:53:22 -0700

    it's effectively done now :D

--- js/datepicker.js
+++ js/datepicker.js
@@ -24,9 +24,41 @@ class DatePicker {
     this.baseMonth = baseDate.getMonth();
     this.baseDate = baseDate.getDate();
 
+    this.makeBar();
+
     this.makeDates();
 
-    this.makeClicky();
+    this.makeInteractive();
+
+    if (options.noUnselected) this.date = new Date();
+  }
+
+  makeBar() {
+    const options = this.options;
+    const bar = document.createElement('div');
+    bar.classList.add('bar');
+
+    if (options.formatMonth) {
+      const monthBar = document.createElement('div');
+      monthBar.classList.add('month-name');
+      bar.appendChild(monthBar);
+      this.monthNameBar = monthBar;
+    } else {
+      this.monthNameBar = null;
+    }
+
+    if (options.dayStrings) {
+      const dayBar = document.createElement('div');
+      dayBar.classList.add('weekday-names');
+      for (let i = 0; i < options.dayStrings.length; i++) {
+        const day = document.createElement('span');
+        day.textContent = options.dayStrings[i];
+        dayBar.appendChild(day);
+      }
+      bar.appendChild(dayBar);
+    }
+
+    this.wrapper.appendChild(bar);
   }
 
   makeDates() {
@@ -36,17 +68,21 @@ class DatePicker {
 
     const svg = document.createElementNS(SVG_NS, 'svg');
     svg.setAttributeNS(null, 'width', options.width);
+    svg.classList.add('calendar');
 
     const dateLabels = document.createElementNS(SVG_NS, 'g');
     dateLabels.style.textAnchor = 'middle';
     dateLabels.style.dominantBaseline = 'middle';
+    dateLabels.classList.add('dates');
 
     const monthHighlights = document.createElementNS(SVG_NS, 'g');
+    dateLabels.classList.add('month-highlights');
 
     const offset = (options.range[0].getDay() + 7 - options.weekStart) % 7;
     let days = this.days + 1;
     let pos = offset - 1;
     let lastMonth = null;
+    this.monthHighlights = [];
     while (days--) {
       pos++;
 
@@ -59,14 +95,16 @@ class DatePicker {
       dateLabels.appendChild(text);
 
       const month = date.getMonth();
+      const year = date.getFullYear();
       if (!lastMonth || month !== lastMonth.month) {
+        const top = Math.floor(pos / 7) * dateHeight;
         lastMonth = {
           month: month,
-          path: `M0 ${Math.floor(pos / 7 + 1) * dateHeight}H${pos % 7 * dateWidth}`
-            + `V${Math.floor(pos / 7) * dateHeight}H${options.width}`
+          top: top,
+          path: `M0 ${top + dateHeight}H${pos % 7 * dateWidth}V${top}H${options.width}`
         };
       }
-      if (dateDate === DatePicker.getMonthLength(date.getFullYear(), month) || days === 0) {
+      if (dateDate === DatePicker.getMonthLength(year, month) || days === 0) {
         lastMonth.path += `V${Math.floor(pos / 7) * dateHeight}H`
           + `${pos % 7 * dateWidth + dateWidth}V${Math.floor(pos / 7 + 1) * dateHeight}`
           + 'H0z';
@@ -74,6 +112,11 @@ class DatePicker {
         const path = document.createElementNS(SVG_NS, 'path');
         path.setAttributeNS(null, 'd', lastMonth.path);
         monthHighlights.appendChild(path);
+        this.monthHighlights.push({
+          name: options.formatMonth ? options.formatMonth(year, month) : '',
+          top: lastMonth.top,
+          path: path
+        });
       }
     }
     svg.setAttributeNS(null, 'height', Math.ceil(pos / 7) * dateHeight);
@@ -81,6 +124,7 @@ class DatePicker {
     const circle = document.createElementNS(SVG_NS, 'circle');
     circle.style.display = 'none';
     circle.setAttributeNS(null, 'r', 10); // TEMP
+    circle.classList.add('selected');
 
     svg.appendChild(circle);
     svg.appendChild(monthHighlights);
@@ -88,20 +132,24 @@ class DatePicker {
     this.wrapper.appendChild(svg);
 
     this.svg = svg;
+    this.dateLabels = dateLabels;
     this.circle = circle;
     this.visualOffset = offset;
   }
 
-  makeClicky() {
+  makeInteractive() {
     const options = this.options;
     const svg = this.svg;
-    this.svg.addEventListener('click', e => {
+    const wrapper = this.wrapper;
+
+    svg.addEventListener('click', e => {
       const box = svg.getBoundingClientRect();
       this.offset = Math.floor((e.clientX - box.left) / options.dateWidth)
         + Math.floor((e.clientY - box.top) / options.dateHeight) * 7
         - this.visualOffset;
     }, false);
-    this.wrapper.addEventListener('keydown', e => {
+
+    wrapper.addEventListener('keydown', e => {
       const date = this.date;
       if (!date) return;
       const month = date.getMonth();
@@ -150,6 +198,25 @@ class DatePicker {
       }
       if (preventScroll) e.preventDefault();
     }, false);
+
+    if (options.formatMonth) {
+      const monthHighlights = this.monthHighlights;
+      const onscroll = e => {
+        const threshold = wrapper.scrollTop + options.dateHeight;
+        let i = monthHighlights.length;
+        while (i--) if (monthHighlights[i].top < threshold) break;
+        if (this.currentHighlight !== i) {
+          if (this.currentHighlight >= 0) {
+            monthHighlights[this.currentHighlight].path.classList.remove('highlight');
+          }
+          this.currentHighlight = i;
+          monthHighlights[i].path.classList.add('highlight');
+          this.monthNameBar.textContent = monthHighlights[i].name;
+        }
+      };
+      onscroll();
+      wrapper.addEventListener('scroll', onscroll, false);
+    }
   }
 
   getDateFromOffset(offset) {
@@ -165,7 +232,7 @@ class DatePicker {
       this.offset = null;
       return;
     }
-    this.offset = (newDate.getTime() - this.options.range[0].getTime()) / MS_PER_DAY;
+    this.offset = Math.floor((newDate.getTime() - this.options.range[0].getTime()) / MS_PER_DAY);
   }
 
   get offset() {
