0c565e222f436563dbab356f0e1f2d37bbea4bff
SheepTester <ussa.say@gmail.com>
2018-06-09 20:33:22 -0700

    forgot to remove the broken code >:(

--- demo/sw-lang-test/sw.js
+++ demo/sw-lang-test/sw.js
@@ -31,7 +31,6 @@ self.addEventListener('fetch', e => {
   }));
 });
 self.addEventListener('activate', e => {
-  client.postMessage('ready');
   e.waitUntil(caches.keys()
     .then(names => Promise.all(names.map(cache => CACHE_NAME !== cache && caches.delete(cache))))
     .then(() => self.clients.claim())
