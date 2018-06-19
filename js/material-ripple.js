154f685dd642793f950a8ef846cbe564abf9ad2f
Sean <ussa.say@gmail.com>
2018-06-18 19:36:57 -0700

    oops

--- js/material-ripple.js
+++ js/material-ripple.js
@@ -87,5 +87,5 @@ function hasMaterialRipple(elem, round) {
 }
 
 document.addEventListener('DOMContentLoaded', e => {
-  document.querySelectorAll('[g=-ripple]').forEach(btn => hasMaterialRipple(btn));
+  document.querySelectorAll('[data-ripple]').forEach(btn => hasMaterialRipple(btn));
 }, {once: true});
