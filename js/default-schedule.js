1ea3e92b0b44bbe9f975b1bef6d0bf73795e66f5
SheepTester <ussa.say@gmail.com>
2018-08-09 15:35:54 -0700

    update schedule - passing period is not part of brunch and lunch anymore

--- js/default-schedule.js
+++ js/default-schedule.js
@@ -2,40 +2,40 @@ const normalSchedules = [
   null,
   [
     {period: "A", start: 505, end: 585},
-    {period: "BRUNCH", start: 585, end: 600},
+    {period: "BRUNCH", start: 585, end: 590},
     {period: "B", start: 600, end: 675},
     {period: "C", start: 685, end: 760},
-    {period: "LUNCH", start: 760, end: 800},
+    {period: "LUNCH", start: 760, end: 790},
     {period: "F", start: 800, end: 875}
   ], [
     {period: "D", start: 505, end: 585},
-    {period: "BRUNCH", start: 585, end: 600},
+    {period: "BRUNCH", start: 585, end: 590},
     {period: "FLEX", start: 600, end: 650},
     {period: "E", start: 660, end: 735},
-    {period: "LUNCH", start: 735, end: 775},
+    {period: "LUNCH", start: 735, end: 765},
     {period: "A", start: 775, end: 855},
     {period: "G", start: 865, end: 940}
   ], [
     {period: "B", start: 505, end: 590},
-    {period: "BRUNCH", start: 590, end: 605},
+    {period: "BRUNCH", start: 590, end: 595},
     {period: "C", start: 605, end: 685},
     {period: "D", start: 695, end: 775},
-    {period: "LUNCH", start: 775, end: 815},
+    {period: "LUNCH", start: 775, end: 805},
     {period: "F", start: 815, end: 895}
   ], [
     {period: "E", start: 505, end: 590},
-    {period: "BRUNCH", start: 590, end: 605},
+    {period: "BRUNCH", start: 590, end: 595},
     {period: "FLEX", start: 605, end: 655},
     {period: "B", start: 665, end: 735},
-    {period: "LUNCH", start: 735, end: 775},
+    {period: "LUNCH", start: 735, end: 765},
     {period: "A", start: 775, end: 845},
     {period: "G", start: 855, end: 935}
   ], [
     {period: "C", start: 505, end: 580},
-    {period: "BRUNCH", start: 580, end: 595},
+    {period: "BRUNCH", start: 580, end: 585},
     {period: "D", start: 595, end: 665},
     {period: "E", start: 675, end: 745},
-    {period: "LUNCH", start: 745, end: 785},
+    {period: "LUNCH", start: 745, end: 775},
     {period: "F", start: 785, end: 855},
     {period: "G", start: 865, end: 935}
   ],
