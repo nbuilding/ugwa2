7b395fc1cc4aa9c8a386c468b0674cce5e872ffa
SheepTester <ussa.say@gmail.com>
2018-10-07 21:09:13 -0700

    less informal default period names; apparently some people use this

--- js/prefs.js
+++ js/prefs.js
@@ -3,21 +3,21 @@
 const Prefs = {
 
   periodData: {
-    ZERO: {name: '0', desc: '', colour: null},
-    A: {name: 'a period', desc: 'yanny howard', colour: [233, 30, 99]},
-    B: {name: 'b period', desc: 'the woz', colour: [255, 193, 7]},
-    C: {name: 'Period C', desc: 'le fup', colour: [103, 58, 183]},
+    ZERO: {name: '0 period', desc: '', colour: null},
+    A: {name: 'A period', desc: 'yanny howard', colour: [233, 30, 99]},
+    B: {name: 'B period', desc: 'the woz', colour: [255, 193, 7]},
+    C: {name: 'C period', desc: 'le fup', colour: [103, 58, 183]},
     D: {name: 'D period', desc: 'TOLENDO TOLENS', colour: [33, 150, 243]},
-    E: {name: 'pe', desc: 'so.... um... so... um....', colour: [139, 195, 74]},
-    F: {name: 'very long class name muahaha', desc: 'vocabphilia', colour: [156, 39, 176]},
-    G: {name: 'españñññññññol', desc: '¡¡¡¿¿¿¿INGLÉS????!!!!', colour: [255, 152, 0]},
-    H: {name: 'h', desc: '', colour: null},
-    BRUNCH: {name: 'brunch lol', desc: 'useful i guess for... idk', colour: null},
-    LUNCH: {name: 'longer longer passing period', desc: 'I WANT TO PLAY SOME MINECRAAAAAFT', colour: null},
-    FLEX: {name: 'unproductive yet free time meant to reduce stress that totally won\'t be replaced with something more stressful and restricting that is also meant to reduce stress', desc: 'ALL SUBMIT TO YUN', colour: [96, 125, 139]},
-    SELF: {name: 'oh. no.', desc: 'DOWN WITH SELF', colour: [96, 125, 139]},
-    STAFF_COLLAB: {name: 'collab', desc: '', colour: [96, 125, 139]},
-    STAFF_MEETINGS: {name: 'meet', desc: '', colour: [96, 125, 139]}
+    E: {name: 'E period', desc: 'so.... um... so... um....', colour: [139, 195, 74]},
+    F: {name: 'F period', desc: 'vocabphilia', colour: [156, 39, 176]},
+    G: {name: 'G period', desc: '¡¡¡¿¿¿¿INGLÉS????!!!!', colour: [255, 152, 0]},
+    H: {name: 'H period', desc: '', colour: null},
+    BRUNCH: {name: 'Brunch', desc: 'useful i guess for... idk', colour: null},
+    LUNCH: {name: 'Lunch', desc: 'I WANT TO PLAY SOME MINECRAAAAAFT', colour: null},
+    FLEX: {name: 'Flex', desc: 'ALL SUBMIT TO YUN', colour: [96, 125, 139]},
+    SELF: {name: 'SELF', desc: 'DOWN WITH SELF', colour: [96, 125, 139]},
+    STAFF_COLLAB: {name: 'Staff collaboration', desc: '', colour: [96, 125, 139]},
+    STAFF_MEETINGS: {name: 'Staff meetings', desc: '', colour: [96, 125, 139]}
   },
 
   options: {
