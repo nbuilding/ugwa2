4b79673fbcb7dfd356caf6d68beea330990e6f17
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-11-03 13:15:17 -0800

    Staff periods in the schedule!

--- js/default-schedule.js
+++ js/default-schedule.js
@@ -1,6 +1,7 @@
 const normalSchedules = [
   null, // Sunday
   [
+    {period: "STAFF_COLLAB", start: 510, end: 600},
     {period: "1", start: 600, end: 630},
     {period: "2", start: 640, end: 670},
     {period: "3", start: 680, end: 710},
@@ -9,8 +10,10 @@ const normalSchedules = [
     {period: "5", start: 795, end: 825},
     {period: "6", start: 835, end: 865},
     {period: "7", start: 875, end: 905},
+    {period: "STAFF_MEETINGS", start: 910, end: 940},
     {period: "8", start: 945, end: 975}
   ], [
+    {period: "STAFF_COLLAB", start: 510, end: 535},
     {period: "1", start: 540, end: 615},
     {period: "2", start: 625, end: 700},
     {period: "LUNCH", start: 700, end: 730},
@@ -19,6 +22,7 @@ const normalSchedules = [
     {period: "OFFICE_HOURS", start: 910, end: 940},
     {period: "8", start: 945, end: 1020}
   ], [
+    {period: "STAFF_MEETINGS", start: 510, end: 575},
     {period: "5", start: 580, end: 655},
     {period: "GUNN_TOGETHER", start: 665, end: 700},
     {period: "LUNCH", start: 700, end: 730},
@@ -26,6 +30,7 @@ const normalSchedules = [
     {period: "7", start: 830, end: 905},
     {period: "OFFICE_HOURS", start: 910, end: 940}
   ], [
+    {period: "STAFF_COLLAB", start: 510, end: 535},
     {period: "1", start: 540, end: 615},
     {period: "2", start: 625, end: 700},
     {period: "LUNCH", start: 700, end: 730},
@@ -34,6 +39,7 @@ const normalSchedules = [
     {period: "OFFICE_HOURS", start: 910, end: 940},
     {period: "8", start: 945, end: 1020}
   ], [
+    {period: "STAFF_COLLAB", start: 510, end: 575},
     {period: "5", start: 580, end: 655},
     {period: "SELF", start: 665, end: 700},
     {period: "LUNCH", start: 700, end: 730},
