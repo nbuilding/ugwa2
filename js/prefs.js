d56b7cc09a5f2c449f71742169e16364a060eff7
mg27713 <68036487+mg27713@users.noreply.github.com>
2020-11-03 12:46:35 -0800

    Periods that match this year.

--- js/prefs.js
+++ js/prefs.js
@@ -4,21 +4,22 @@ const roundnessClasses = ['sharp', 'roundish', 'round'];
 const Prefs = {
 
   periodData: {
-    ZERO: {name: '0 period', desc: '', colour: null},
-    A: {name: 'A period', desc: '', colour: [233, 30, 99]},
-    B: {name: 'B period', desc: '', colour: [255, 193, 7]},
-    C: {name: 'C period', desc: '', colour: [103, 58, 183]},
-    D: {name: 'D period', desc: '', colour: [33, 150, 243]},
-    E: {name: 'E period', desc: '', colour: [139, 195, 74]},
-    F: {name: 'F period', desc: '', colour: [156, 39, 176]},
-    G: {name: 'G period', desc: '', colour: [255, 152, 0]},
-    H: {name: 'H period', desc: '', colour: null},
-    BRUNCH: {name: 'Brunch', desc: '', colour: null},
+//     ZERO: {name: '0 period', desc: '', colour: null}, // no 0 period this year!
+    1: {name: 'Period 1', desc: '', colour: [233, 30, 99]},
+    2: {name: 'Period 2', desc: '', colour: [255, 193, 7]},
+    3: {name: 'Period 3', desc: '', colour: [103, 58, 183]},
+    4: {name: 'Period 4', desc: '', colour: [33, 150, 243]},
+    5: {name: 'Period 5', desc: '', colour: [139, 195, 74]},
+    6: {name: 'Period 6', desc: '', colour: [156, 39, 176]},
+    7: {name: 'Period 7', desc: '', colour: [255, 152, 0]},
+    8: {name: 'Period 8', desc: '', colour: null},
+//     BRUNCH: {name: 'Brunch', desc: '', colour: null}, // no brunch this year either
     LUNCH: {name: 'Lunch', desc: '', colour: null},
-    FLEX: {name: 'Flex', desc: '', colour: [96, 125, 139]},
+    OFFICE_HOURS: {name: 'Office Hours', desc: '', colour: [96, 125, 139]},
     SELF: {name: 'SELF', desc: '', colour: [96, 125, 139]},
-    STAFF_COLLAB: {name: 'Staff collaboration', desc: '', colour: [96, 125, 139]},
-    STAFF_MEETINGS: {name: 'Staff meetings', desc: '', colour: [96, 125, 139]}
+    GUNN_TOGETHER: {name: 'Gunn Together', desc: '', colour: null, gt: true} // TODO: treat GT specially.
+//     STAFF_COLLAB: {name: 'Staff collaboration', desc: '', colour: [96, 125, 139]}, // staff stuff left out for now
+//     STAFF_MEETINGS: {name: 'Staff meetings', desc: '', colour: [96, 125, 139]}
   },
 
   options: {
